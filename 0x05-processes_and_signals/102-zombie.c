9a776a75f4e6b7d7247e172cb61183a20e014bb3
simone mwangi <mwanjiis4@gmail.com>
2022-07-23 00:34:50 +0300

    Committing...

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,19 +3,20 @@
 #include <unistd.h>
 
 /**
- * infinite_while - create an infinite loop
+ * infinite_while - creates an infinite loop to make the program hang
  * Return: always 0
  */
 int infinite_while(void)
 {
 	while (1)
+	{
 		sleep(1);
-
+	}
 	return (0);
 }
 
 /**
- * main - create 5 zombie processes
+ * main - creates 5 zombie processes
  * Return: always 0
  */
 int main(void)
--- 0x05-processes_and_signals/README.md
+++ 0x05-processes_and_signals/README.md
@@ -1,1 +1,1 @@
-# 5 Processes and signals
+# 0x05. Processes and signals
